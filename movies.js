b528e8b294aa16dd2a30a00fbb1da43b474eca83
erikakasperaviciute <erika.kasperaviciute@hotmail.lt>
2023-09-28 11:36:13 +0300

    movies order fixed

--- movies.js
+++ movies.js
@@ -11,59 +11,58 @@ function getMovies() {
   fetch("movies.json")
     .then((response) => response.json())
     .then((moviesId) => {
-      //   console.log(movies);
-      moviesId.forEach((movieId) => {
-        fetch(`movie-${movieId}.json`)
-          .then((response) => response.json())
-          .then((movieInfo) => {
-            // console.log(movieInfo);
-            let title = movieInfo.title;
-            // console.log(title);
-            let rating = movieInfo.rating;
-            let amountOfRatings = movieInfo.amountOfRatings;
-            let yearOfRelease = movieInfo.yearOfRelease;
-            let description = movieInfo.description;
-            let genreArr = movieInfo.genre;
-            let director = movieInfo.director;
-            let starsArr = movieInfo.stars;
+      const allPromises = moviesId.map((movieId) =>
+        fetch(`movie-${movieId}.json`).then((response) => response.json())
+      );
+      return Promise.all(allPromises);
+    })
+    .then((movieInfo) => {
+      movieInfo.forEach((movie) => {
+        let title = movie.title;
+        let rating = movie.rating;
+        let amountOfRatings = movie.amountOfRatings;
+        let yearOfRelease = movie.yearOfRelease;
+        let description = movie.description;
+        let genreArr = movie.genre;
+        let director = movie.director;
+        let starsArr = movie.stars;
 
-            let movieCard = document.createElement("div");
-            mainContainer.append(movieCard);
-            let movieTitle = document.createElement("h2");
-            movieTitle.textContent = title;
-            let movieRating = document.createElement("h4");
-            movieRating.textContent = `Rating: ${rating}/10. Rated ${amountOfRatings} times.`;
-            let movieYear = document.createElement("h4");
-            movieYear.textContent = `Released in ${yearOfRelease}`;
-            let movieDescrpition = document.createElement("p");
-            movieDescrpition.textContent = description;
-            let genreList = document.createElement("ul");
-            genreArr.forEach((genre) => {
-              let genreItem = document.createElement("li");
-              genreItem.textContent = genre;
-              genreList.append(genreItem);
-            });
-            let movieDirector = document.createElement("h4");
-            movieDirector.textContent = `Director: ${director}`;
-            let starsTitle = document.createElement("h4");
-            starsTitle.textContent = "Main stars:";
-            let starsList = document.createElement("ul");
-            starsArr.forEach((star) => {
-              let starItem = document.createElement("li");
-              starItem.textContent = star;
-              starsList.append(starItem);
-            });
-            movieCard.append(
-              movieTitle,
-              movieRating,
-              movieYear,
-              movieDescrpition,
-              genreList,
-              movieDirector,
-              starsTitle,
-              starsList
-            );
-          });
+        let movieCard = document.createElement("div");
+        mainContainer.append(movieCard);
+        let movieTitle = document.createElement("h2");
+        movieTitle.textContent = title;
+        let movieRating = document.createElement("h4");
+        movieRating.textContent = `Rating: ${rating}/10. Rated ${amountOfRatings} times.`;
+        let movieYear = document.createElement("h4");
+        movieYear.textContent = `Released in ${yearOfRelease}`;
+        let movieDescrpition = document.createElement("p");
+        movieDescrpition.textContent = description;
+        let genreList = document.createElement("ul");
+        genreArr.forEach((genre) => {
+          let genreItem = document.createElement("li");
+          genreItem.textContent = genre;
+          genreList.append(genreItem);
+        });
+        let movieDirector = document.createElement("h4");
+        movieDirector.textContent = `Director: ${director}`;
+        let starsTitle = document.createElement("h4");
+        starsTitle.textContent = "Main stars:";
+        let starsList = document.createElement("ul");
+        starsArr.forEach((star) => {
+          let starItem = document.createElement("li");
+          starItem.textContent = star;
+          starsList.append(starItem);
+        });
+        movieCard.append(
+          movieTitle,
+          movieRating,
+          movieYear,
+          movieDescrpition,
+          genreList,
+          movieDirector,
+          starsTitle,
+          starsList
+        );
       });
     });
 }
